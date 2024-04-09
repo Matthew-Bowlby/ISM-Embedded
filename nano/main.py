0d5f3353e4240c117c1e307d952003af9b4e793b
Chris McKinnon <cmckin18@vols.utk.edu>
2024-04-09 15:38:54 -0400

    separated db code and added pwm for lights

--- nano/main.py
+++ nano/main.py
@@ -1,14 +1,14 @@
 import eel
 import RPi.GPIO as GPIO
-import sqlite3
 import sys
 import json
 import numpy as np
+from database import DB
+light_pin = 32
+motion_pin = 40
+target_duty=0
+duty = 0
 
-light_pin = 19
-motion_pin = 12
-current_duty=0
-target_duty =0
 eel.init("web")
 
 login_status = False
@@ -16,109 +16,50 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(motion_pin, GPIO.IN)
 GPIO.setup(light_pin, GPIO.OUT)
 
+light_pwm = GPIO.PWM(light_pin, 150)
+
+
+db = DB()
+
 print("setup pins")
 
-try:
-    sqliteConnector = sqlite3.connect("user_data.db")
-    cursor = sqliteConnector.cursor()
 
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS user_data(
-            NAME TEXT NOT NULL,
-            TEMP REAL,
-            VANITY INTEGER
-                            
-
-    );"""
-    )
-except sqlite3.Error as error:
-    print(f"Error occured: {error}")
-    sys.exit(1)
 print("setup db")
 
 
 def runFacialRecognition():
     global login_status
-    name = "JP"
-    cnt = cursor.execute(f"SELECT * from user_data WHERE NAME='{name}'")
-
-    columns = [column[0] for column in cnt.description]
-    out = cnt.fetchone()
-
-    json_data = []
-    row_data = {}
-    for i in range(len(columns)):
-        row_data[columns[i]] = out[i]
-    json_data.append(row_data)
-    json_output = json.dumps(json_data)
-    eel.loginEvent(json_output)
+    # run facial rec and return user #
+    user = 1
+    userinfo=db.getUserData(1)
+    
+    eel.loginEvent(userinfo)
 
     login_status = True
 
 
 def turn_on():
-    GPIO.output(light_pin, GPIO.HIGH)
+    global duty
+    global target_duty
+    while duty<target_duty:
+        duty += 1
+        light_pwm.ChangeDutyCycle(duty)
+        eel.sleep(0.01)
 
 
 def turn_off():
-    GPIO.output(light_pin, GPIO.LOW)
-
-def lightScale(dir):
+    global duty
     global target_duty
-    global current_duty
-
-    if dir:
-        for x in np.linspace(current_duty, target_duty, 50, dtype=int):
-            current_duty = x
-    else:
-        for x in np.linspace(target_duty, current_duty, 50, dtype=int):
-            current_duty = x
-def turnOnLights():
-    global current_duty
-    freq = 120  # for 120 hz
-    current_duty = 0  # 1% of the period, the voltage must be high.
-
-    period = 1 / freq
-
-    # so the the duration for which the voltage will be high
-    high_period = period * current_duty / 100  # percentange of period the signal is high
-    low_period = period - high_period
-
-    while True:
-        try:
-            turn_on()
-            eel.sleep(high_period)
-            turn_off()
-            eel.sleep(low_period)
-        except KeyboardInterrupt:
-            break
-
-
-def turnOffLights():
-    freq = 120  # for 120 hz
-    global current_duty
-
-    period = 1 / freq
-
-    # so the the duration for which the voltage will be high
-    high_period = period * current_duty / 100  # percentange of period the signal is high
-    low_period = period - high_period
-
-    while current_duty>0:
-        try:
-            turn_on()
-            eel.sleep(high_period)
-            turn_off()
-            eel.sleep(low_period)
-        except KeyboardInterrupt:
-            break
-    turn_off()
+    while duty>0:
+        duty -= 1
+        light_pwm.ChangeDutyCycle(duty)
+        eel.sleep(0.01)
 
 
 def screenControl():
     global login_status
-    lightControl=None
-    frControl=None
+    lightControl = None
+    frControl = None
     prev_val = None
     timeout_count = 0
     try:
@@ -130,32 +71,32 @@ def screenControl():
                     if not login_status:
                         print("Motion detected!")
                         eel.wakeEvent()
-                        lightControl= eel.spawn(turnOnLights)
-                        frControl=eel.spawn(runFacialRecognition)
+                        eel.spawn(turn_on)
+                        #frControl = eel.spawn(runFacialRecognition)
                     else:
                         timeout_count = 0
-
-                else:
-                    if not login_status:
-                        eel.sleepEvent()
-                        lightControl.kill()
-                        frControl.kill()
-                        eel.spawn(turnOffLights)
+                       
             elif val == GPIO.LOW:
+                timeout_count += 1
                 if login_status:
-                    timeout_count += 1
                     if timeout_count == 60:
                         eel.sleepEvent()
                         login_status = False
                         timeout_count = 0
-                        eel.spawn(turnOffLights)
+                        eel.spawn(turn_off)
+                else:
+                    if timeout_count == 10:
+                        eel.sleepEvent()
+                        eel.spawn(turn_off)
+                        frControl.kill()
 
             prev_val = val
             eel.sleep(1)
     finally:
         GPIO.cleanup()
 
-
+target_duty=DB.getUserVanity(0)
+light_pwm.start(duty)
 eel.spawn(screenControl)
 print("starting")
 
